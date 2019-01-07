dc3bc4373c5488e88a7766f994e71cf5b70ec7f3
T3H <dathalongbay@gmail.com>
2019-01-07 18:43:28 +0700

    abc

---
+++ 001-request/index2.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: T3H
+ * Date: 1/7/2019
+ * Time: 6:30 PM
+ */
+
+echo '<pre>';
+print_r($_REQUEST);
+echo '</pre>';
+
+echo 'Tong :' . $val =$_REQUEST['thamso1'] + $_REQUEST['thamso2'];
+echo '<br>';
+echo 'Hieu :'. $val1 =$_REQUEST['thamso1'] - $_REQUEST['thamso2'];
+echo '<br>';
+echo 'Tich :'. $val2 =$_REQUEST['thamso1'] * $_REQUEST['thamso2'];
+echo '<br>';
+echo 'Thuong :'. $val3 =$_REQUEST['thamso1'] / $_REQUEST['thamso2'];
