508507879d005cb21bfb3df8c70294ce6cdcda0c
T3H <dathalongbay@gmail.com>
2019-01-07 18:55:57 +0700

    abc

---
+++ 001-request/index3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<form name="convertdungluong" action="" method="post">
+    <P>
+        <label>Gigabyte :</label>
+        <input type="text" name="capacity" value="" placeholder="Nhập vào dung lượng cần chuyển từ gigabyte">
+    </P>
+</form>
+
+<?php
+/**
+ * Chuyển từ gigabyte
+ * 1 - thành megabyte
+ * 2 - thành byte
+ * 3 - thành bit
+ */
+
+echo '<pre>';
+print_r($_REQUEST);
+echo '</pre>';
+?>
+
+</body>
+</html>
