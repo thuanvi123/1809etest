1b1abcf598c15b62c28fe2b8b9986a6f05ac7703
dathubt <dathalongbay@gmail.com>
2019-01-11 19:06:24 +0700

    Add files via upload

---
+++ quick_test/0001/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+    <h1>1 . Viết ra 1 chương trình mà khi click vào button bên dưới sẽ tạo ra 100 thư mục
+    có tên từ 001 đến 100 bên trong thư mục demo của file này</h1>
+
+    <h1>2 . Sau đó tạo 1 file có tên là index.php trong mỗi thư mục đó
+    trong mỗi file này có 1 câu lệnh php in ra tên file hiện tại</h1>
+
+    <h1><a href="#">Click tạo thư mục tự động</a></h1>
+
+</body>
+</html>
